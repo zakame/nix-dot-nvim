912ac9962d6b8d77037fd3508405b6403a62b57c
Zak B. Elep <zakame@zakame.net>
2024-10-26 17:38:27 +0800

    feat: add tokyonight theme

--- nix/neovim-overlay.nix
+++ nix/neovim-overlay.nix
@@ -82,6 +82,7 @@ with final.pkgs.lib; let
     # (mkNvimPlugin inputs.wf-nvim "wf.nvim") # (example) keymap hints | https://github.com/Cassin01/wf.nvim
     # ^ bleeding-edge plugins from flake inputs
     which-key-nvim
+    tokyonight-nvim
   ];
 
   extraPackages = with pkgs; [
---
+++ nvim/plugin/tokyonight.lua
@@ -0,0 +1,6 @@
+if vim.g.did_load_tokyonight_plugin then
+  return
+end
+vim.g.did_load_tokyonight_plugin = true
+
+vim.cmd[[colorscheme tokyonight]]
