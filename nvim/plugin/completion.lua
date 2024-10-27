f799d46cb03e24ef9a4f12834d76f3fd1177939b
Zak B. Elep <zakame@zakame.net>
2024-10-27 22:55:00 +0800

    feat: use ellipsis single-char in completion popups

--- nvim/plugin/completion.lua
+++ nvim/plugin/completion.lua
@@ -34,7 +34,7 @@ cmp.setup {
       mode = 'symbol_text',
       with_text = true,
       maxwidth = 50, -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
-      ellipsis_char = '...', -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
+      ellipsis_char = '…', -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
 
       menu = {
         buffer = '[BUF]',
