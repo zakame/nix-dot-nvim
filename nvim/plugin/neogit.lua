147a73348a3cbd5a2f36ce480c7b74a97285f5fd
Zak B. Elep <zakame@zakame.net>
2024-10-29 01:22:33 +0800

    feat: reconfigure Neogit and keymap

--- nvim/plugin/neogit.lua
+++ nvim/plugin/neogit.lua
@@ -5,25 +5,4 @@ vim.g.did_load_neogit_plugin = true
 
 local neogit = require('neogit')
 
-neogit.setup {
-  disable_builtin_notifications = true,
-  disable_insert_on_commit = 'auto',
-  integrations = {
-    diffview = true,
-    telescope = true,
-    fzf_lua = true,
-  },
-  sections = {
-    ---@diagnostic disable-next-line: missing-fields
-    recent = {
-      folded = false,
-    },
-  },
-}
-vim.keymap.set('n', '<leader>go', neogit.open, { noremap = true, silent = true, desc = 'neo[g]it [o]pen' })
-vim.keymap.set('n', '<leader>gs', function()
-  neogit.open { kind = 'auto' }
-end, { noremap = true, silent = true, desc = 'neo[g]it open [s]plit' })
-vim.keymap.set('n', '<leader>gc', function()
-  neogit.open { 'commit' }
-end, { noremap = true, silent = true, desc = 'neo[g]it [c]ommit' })
+vim.keymap.set('n', '<leader>hN', neogit.open, { noremap = true, silent = true, desc = 'git open [N]eogit' })
